92bff89a84e95a18c0a4e315e4f55fed37a5d6c3
Tshepang Lekhonkhobe <tshepang@gmail.com>
2017-05-31 07:56:18 +0200

    example: a reminder that usize is assumed to be 8 bytes long

--- examples/basic.rs
+++ examples/basic.rs
@@ -18,7 +18,7 @@ fn main() {
 
     let encoded: Vec<u8> = serialize(&world, Infinite).unwrap();
 
-    // 8 bytes for the length of the vector, 4 bytes per float.
+    // 8 bytes for the length of the vector (usize), 4 bytes per float.
     assert_eq!(encoded.len(), 8 + 4 * 4);
 
     let decoded: World = deserialize(&encoded[..]).unwrap();
