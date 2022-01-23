82ab6e309b395b5871d269b39e1eb5f0be3e7e60
Victor Koenders <gpg@trangar.com>
2022-01-23 12:03:30 +0100

    Added decode/deserialize test to test_same

--- compatibility/src/lib.rs
+++ compatibility/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg(test)]
 
+use ::rand::Rng;
 use bincode_1::Options;
 
 mod rand;
@@ -7,9 +8,14 @@ mod sway;
 
 pub fn test_same_with_config<T, C, O>(t: &T, bincode_1_options: O, bincode_2_config: C)
 where
-    T: bincode_2::Encode + serde::Serialize + core::fmt::Debug,
-    C: bincode_2::config::Config + Clone,
-    O: bincode_1::Options + Clone,
+    T: bincode_2::Encode
+        + bincode_2::Decode
+        + serde::Serialize
+        + serde::de::DeserializeOwned
+        + core::fmt::Debug
+        + PartialEq,
+    C: bincode_2::config::Config,
+    O: bincode_1::Options + Copy,
 {
     let bincode_1_output = bincode_1_options.serialize(t).unwrap();
     let bincode_2_output = bincode_2::encode_to_vec(t, bincode_2_config).unwrap();
@@ -19,11 +25,30 @@ where
         "{:?} serializes differently",
         t
     );
+
+    let decoded: T = bincode_1_options.deserialize(&bincode_1_output).unwrap();
+    assert_eq!(&decoded, t);
+    let decoded: T = bincode_1_options.deserialize(&bincode_2_output).unwrap();
+    assert_eq!(&decoded, t);
+
+    let decoded: T = bincode_2::decode_from_slice(&bincode_1_output, bincode_2_config)
+        .unwrap()
+        .0;
+    assert_eq!(&decoded, t);
+    let decoded: T = bincode_2::decode_from_slice(&bincode_2_output, bincode_2_config)
+        .unwrap()
+        .0;
+    assert_eq!(&decoded, t);
 }
 
 pub fn test_same<T>(t: T)
 where
-    T: bincode_2::Encode + serde::Serialize + core::fmt::Debug,
+    T: bincode_2::Encode
+        + bincode_2::Decode
+        + serde::Serialize
+        + serde::de::DeserializeOwned
+        + core::fmt::Debug
+        + PartialEq,
 {
     test_same_with_config(
         &t,
@@ -61,4 +86,13 @@ where
             .with_little_endian()
             .with_fixed_int_encoding(),
     );
+}
+
+pub fn gen_string(rng: &mut impl Rng) -> String {
+    let len = rng.gen_range(0..100usize);
+    let mut result = String::with_capacity(len * 4);
+    for _ in 0..len {
+        result.push(rng.gen_range('\0'..char::MAX));
+    }
+    result
 }
