9f2f3e45fbfcb32e08675a975250b00dd17d3832
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2022-02-06 14:15:35 +0000

    Resolve missing info

--- compatibility/src/membership.rs
+++ compatibility/src/membership.rs
@@ -1,3 +1,7 @@
+// Added by [ppamorim](https://github.com/ppamorim)
+// Taken from https://github.com/datafuselabs/openraft/blob/209ae677ade5b624fea9f6630e9ff191963f5d74/openraft/src/membership/membership.rs#L21
+// License: Openraft is licensed under the terms of the MIT License or the Apache License 2.0, at your choosing.
+
 use std::collections::BTreeSet;
 use rand::{prelude::ThreadRng, Rng};
 
@@ -23,24 +27,24 @@ pub fn test() {
     let mut rng = rand::thread_rng();
     for _ in 0..1000 {
         crate::test_same(Membership {
-            learners: random_btreeset(&mut rng, 1000),
+            learners: random_btreeset(&mut rng),
             configs: vec_random_btreeset(&mut rng,),
-            all_members: random_btreeset(&mut rng, 1000),
+            all_members: random_btreeset(&mut rng),
         });
     }
 }
 
 fn vec_random_btreeset(rng: &mut ThreadRng) -> Vec<BTreeSet<NodeId>> {
   let mut vec = Vec::with_capacity(5);
-  for _ in 0..10 {
-    vec.push(random_btreeset(rng, 100));
+  for _ in 0..rng.gen_range(0..10) {
+    vec.push(random_btreeset(rng));
   }
   vec
 }
 
-fn random_btreeset(rng: &mut ThreadRng, count: u32) -> BTreeSet<NodeId> {
+fn random_btreeset(rng: &mut ThreadRng) -> BTreeSet<NodeId> {
   let mut set = BTreeSet::new();
-  for _ in 0..count {
+  for _ in 0..rng.gen_range(0..100) {
     let v = rng.gen();
     set.insert(v);
   }
