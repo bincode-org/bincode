6ecb853db0e0635438d4006723c34f0353fb7f4c
Maciej Hirsz <hello@maciej.codes>
2022-06-15 18:30:24 +0100

    fmt

--- tests/error_size.rs
+++ tests/error_size.rs
@@ -2,17 +2,17 @@
 
 #[test]
 fn decode_error_size() {
-	assert_eq!(std::mem::size_of::<bincode::error::DecodeError>(), 32);
+    assert_eq!(std::mem::size_of::<bincode::error::DecodeError>(), 32);
 }
 
 #[test]
 fn encode_error_size() {
-	#[cfg(feature = "std")]
-	assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 40);
+    #[cfg(feature = "std")]
+    assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 40);
 
-	#[cfg(all(feature = "alloc", not(feature = "std")))]
-	assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 32);
+    #[cfg(all(feature = "alloc", not(feature = "std")))]
+    assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 32);
 
-	#[cfg(not(any(feature = "std", feature = "alloc")))]
-	assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 24);
+    #[cfg(not(any(feature = "std", feature = "alloc")))]
+    assert_eq!(std::mem::size_of::<bincode::error::EncodeError>(), 24);
 }
