86ea6aee61f5b96d71ab2e9d46d52eecbd98a38e
Zoey Riordan <zoey@dos.cafe>
2025-03-05 20:36:44 +0100

    clippy damn you

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -160,7 +160,7 @@ pub trait Decoder: Sealed {
     fn context(&mut self) -> &mut Self::Context;
 
     /// Wraps decoder with a context
-    fn with_context<'a, C>(&'a mut self, context: C) -> WithContext<'a, Self, C> {
+    fn with_context<C>(&mut self, context: C) -> WithContext<Self, C> {
         WithContext {
             decoder: self,
             context,
